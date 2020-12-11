e8abcb394b9decea9882c34d5898caaef184cabb
LuisAngel <luisangel.salvat@gmail.com>
2020-12-10 22:39:50 -0500

    Actualización de App Patitas

--- app/src/main/java/com/qbo/appkea2patitas/ui/MainActivity.kt
+++ app/src/main/java/com/qbo/appkea2patitas/ui/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.qbo.appkea2patitas.ui
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -13,15 +17,23 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.qbo.appkea2patitas.R
+import com.qbo.appkea2patitas.viewmodel.PersonaViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
+    private lateinit var navView: NavigationView
+    private lateinit var personaViewModel: PersonaViewModel
+    private lateinit var preferencias : SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        preferencias = getSharedPreferences("appPatitas", MODE_PRIVATE)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
@@ -31,7 +43,7 @@ class MainActivity : AppCompatActivity() {
                     .setAction("Action", null).show()
         }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
+        navView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -40,6 +52,36 @@ class MainActivity : AppCompatActivity() {
         ), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        mostrarInformacionLogin()
+    }
+
+    private fun mostrarInformacionLogin(){
+        val tvnomusuarioh: TextView = navView.getHeaderView(0)
+            .findViewById(R.id.tvnomusuarioh)
+        val tvemailusuarioh: TextView = navView.getHeaderView(0)
+            .findViewById(R.id.tvemailusuarioh)
+        personaViewModel = ViewModelProvider(this)
+            .get(PersonaViewModel::class.java)
+        personaViewModel.obtener()
+            .observe(this, Observer { persona->
+                persona?.let {
+                    tvnomusuarioh.text = persona.nombres
+                    tvemailusuarioh.text = persona.email
+                }
+            })
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val itemcerrar = item.itemId
+        if(itemcerrar == R.id.action_cerrar){
+            if(!preferencias.getBoolean("recordardatos", false)){
+                personaViewModel.eliminarTodo()
+            }
+            startActivity(Intent(this,
+                LoginActivity::class.java))
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/qbo/appkea2patitas/ui/RegistrarVoluntarioFragment.kt
+++ app/src/main/java/com/qbo/appkea2patitas/ui/RegistrarVoluntarioFragment.kt
@@ -1,19 +1,102 @@
 package com.qbo.appkea2patitas.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.google.android.material.snackbar.Snackbar
 import com.qbo.appkea2patitas.R
+import com.qbo.appkea2patitas.db.entity.PersonaEntity
+import com.qbo.appkea2patitas.viewmodel.PersonaViewModel
+import kotlinx.android.synthetic.main.fragment_registrar_voluntario.*
+import org.json.JSONObject
 
 
 class RegistrarVoluntarioFragment : Fragment() {
 
+    private lateinit var colapeticiones : RequestQueue
+    private lateinit var personaViewModel: PersonaViewModel
+    private lateinit var objpersona: PersonaEntity
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registrar_voluntario, container, false)
+        colapeticiones = Volley.newRequestQueue(context)
+        personaViewModel = ViewModelProvider(this)
+            .get(PersonaViewModel::class.java)
+        personaViewModel.obtener()
+            .observe(viewLifecycleOwner, Observer { persona->
+                persona?.let {
+                    if(persona.esvoluntario == "1"){
+                        actualizarFragmento()
+                    }else{
+                        objpersona = persona
+                    }
+                }
+            })
+        val vista = inflater.inflate(R.layout.fragment_registrar_voluntario, container, false)
+        val btnregistrarvoluntario : Button = vista
+            .findViewById(R.id.btnregistrarvoluntario)
+        btnregistrarvoluntario.setOnClickListener {
+            if(cbaceptarterminos.isChecked){
+                btnregistrarvoluntario.isEnabled = false
+                registrarVoluntarioApiRest(it)
+            }else{
+                mostrarMensaje(it, getString(R.string.valerrorvoluntario))
+            }
+        }
+        return vista
+    }
+
+    private fun registrarVoluntarioApiRest(vista: View) {
+        val urlapivoluntario = "http://www.kreapps.biz/patitas/personavoluntaria.php"
+        val parametro = JSONObject()
+        parametro.put("idpersona", objpersona.id)
+        val request = JsonObjectRequest(
+            Request.Method.POST,
+            urlapivoluntario,
+            parametro,
+            {response->
+                if(response.getBoolean("rpta")){
+                    val nuevaPersona = PersonaEntity(
+                        objpersona.id,
+                        objpersona.nombres,
+                        objpersona.apellidos,
+                        objpersona.email,
+                        objpersona.celular,
+                        objpersona.usuario,
+                        objpersona.password,
+                        "1"
+                    )
+                    personaViewModel.actualizar(nuevaPersona)
+                    actualizarFragmento()
+                }
+                mostrarMensaje(vista, response.getString("mensaje"))
+                btnregistrarvoluntario.isEnabled = true
+            },{
+                Log.e("ErrorVoluntario", it.message.toString())
+                btnregistrarvoluntario.isEnabled = true
+            })
+        colapeticiones.add(request)
+    }
+
+    private fun actualizarFragmento(){
+        tvtextovoluntario.visibility = View.GONE
+        btnregistrarvoluntario.visibility = View.GONE
+        cbaceptarterminos.visibility = View.GONE
+        tvvoluntario.text = getString(R.string.valtituloesvoluntario)
+    }
+
+    private fun mostrarMensaje(vista: View, mensaje: String) {
+        Snackbar.make(vista, mensaje, Snackbar.LENGTH_LONG).show()
     }
 
 }
